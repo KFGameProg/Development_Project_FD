63b2548c181f19adee780192b537c79d1f83cd8d
ReconCS4 <80109230+ReconCS4@users.noreply.github.com>
2023-02-01 20:18:48 +0000

    Initial implementation of the Basic system

---
+++ vendor/spdlog/out/build/x64-Debug (default)/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/3rd Year/Game Engine Architecture/Assessment/vendor/spdlog
+# Build directory: C:/3rd Year/Game Engine Architecture/Assessment/vendor/spdlog/out/build/x64-Debug (default)
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
---
+++ vendor/spdlog/out/build/x64-Debug (default)/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/3rd Year/Game Engine Architecture/Assessment/vendor/spdlog/tests
+# Build directory: C:/3rd Year/Game Engine Architecture/Assessment/vendor/spdlog/out/build/x64-Debug (default)/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(spdlog-utests "C:/3rd Year/Game Engine Architecture/Assessment/vendor/spdlog/out/build/x64-Debug (default)/tests/spdlog-utests.exe")
+set_tests_properties(spdlog-utests PROPERTIES  _BACKTRACE_TRIPLES "C:/3rd Year/Game Engine Architecture/Assessment/vendor/spdlog/tests/CMakeLists.txt;23;add_test;C:/3rd Year/Game Engine Architecture/Assessment/vendor/spdlog/tests/CMakeLists.txt;0;")
